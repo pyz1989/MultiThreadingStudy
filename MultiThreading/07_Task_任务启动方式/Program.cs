b647765889984cf9f79419bd90b7a406702d8285
pyz1989 <pengyuanzhi@gmail.com>
2021-05-18 18:23:59 +0800

    Add MultiThreading Project

---
+++ MultiThreading/07_Task_任务启动方式/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _07_Task_任务启动方式
+{
+    class Program
+    {
+        /*
+         *  一、Task 【.Net 4.0】
+         *   为什么要有Task？
+         *      Task => Thread  + ThreadPoll + 优化和功能扩展
+         *      Thread: 容易造成时间 + 空间开销，而且使用不当，容易造成线程过多，导致时间片切换...
+         *      ThreadPool: 控制能力比较弱，做Thread的延续，阻塞，取消，超时等等功能, 控制权在CLR，而不是在我们这里...
+         *      Task 看起来像是一个Thread
+         *      Task 是在ThreadPool的基础上进行的封装
+         *      .Net 4.0之后，微软是极力的推荐 Task 来作为异步计算
+         *  二、 Task的启动方式
+         *   1. 实例化启动Task
+         *   2. TaskFactory的方式启动Task
+         *   3. Task.Run方法
+         *   4. Task的同步方法
+         *  三、Task是建立在ThreadPool上面的吗？
+         *    我们的Task底层都是由不同的TaskScheduler支撑的。。。
+         *    TaskScheduler 相当于Task的CPU处理器。。。
+         *    默认的TaskScheduler是ThreadPoolTaskScheduler。。。
+         *    WPF中的TaskScheduler是 SynchronizationContextTaskScheduler
+         *    
+         *    ThreadPoolTaskScheduler
+         *    m_taskScheduler.InternalQueueTask(this);
+         *    大家也可以自定义一些TaskScheduler。。。。
+         *    
+         *    
+                private static readonly TaskScheduler s_defaultTaskScheduler = new ThreadPoolTaskScheduler();
+                // System.Threading.Tasks.ThreadPoolTaskScheduler
+                protected internal override void QueueTask(Task task)
+                {
+	                TaskCreationOptions options = task.Options;
+	                if ((options & TaskCreationOptions.LongRunning) != 0)
+	                {
+		                Thread thread = new Thread(s_longRunningThreadWork);
+		                thread.IsBackground = true;
+		                thread.Start(task);
+	                }
+	                else
+	                {
+		                bool preferLocal = (options & TaskCreationOptions.PreferFairness) == TaskCreationOptions.None;
+		                ThreadPool.UnsafeQueueUserWorkItemInternal(task, preferLocal);
+	                }
+                }
+        *  四、Task<TResult>
+        *   让Task具有返回值，它的父类其实就是Task
+        *   具体的启动方式和Task是一样的
+        */
+        static void Main(string[] args)
+        {
+            // 1. 实例化启动Task
+            Demo_1();
+            // 2. TaskFactory的方式启动Task
+            Demo_2();
+            // 3. Task.Run方法
+            Demo_3();
+            // 4. Task的同步方法,也就是阻塞执行
+            Demo_4();
+
+            // 让Task具有返回值：Task<TResult>
+            Demo_5();
+            Console.WriteLine($"我是主线程【0】: tid = {Thread.CurrentThread.ManagedThreadId}, {DateTime.Now}");
+            Console.ReadKey();
+        }
+
+
+        public static void Demo_1()
+        {
+            Task t1 = new Task(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"我是工作线程【1】: tid = {Thread.CurrentThread.ManagedThreadId}, {DateTime.Now}");
+            });
+            t1.Start();
+        }
+
+        public static void Demo_2()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"我是工作线程【2】: tid = {Thread.CurrentThread.ManagedThreadId}, {DateTime.Now}");
+            });
+        }
+
+        public static void Demo_3()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"我是工作线程【3】: tid = {Thread.CurrentThread.ManagedThreadId}, {DateTime.Now}");
+            });
+        }
+
+        public static void Demo_4()
+        {
+            Task t1 = new Task(() =>
+            {
+                Console.WriteLine($"我是工作线程【4】: tid = {Thread.CurrentThread.ManagedThreadId}, {DateTime.Now}");
+            });
+            t1.RunSynchronously();
+        }
+
+        public static void Demo_5()
+        {
+            // 前后要一致，否则无Result方法
+            Task<string> t1 = new Task<string>(() =>
+            {
+                return "Hello World!";
+            });
+            t1.Start();
+
+            var msg = t1.Result;
+            Console.WriteLine($"收到消息：{msg}");
+        }
+    }
+}
---
+++ MultiThreading/08_Task_任务的延续和阻塞/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _08_Task_任务的延续和阻塞
+{
+    /*
+     *  1. 阻塞
+     *  Thread  => Join方法【让调用线程阻塞】
+     *      参考： Demo_1
+     *  Task
+     *      - WaitAll方法：必须所有的Task执行完才算完成
+     *      - WaitAny方法：只要其中一个Task执行完成就算完成
+     *      
+     *      Task.Wait方法：等待操作
+     *      上面这些等待操作，返回值都是void...
+     *  2. 延续    
+     *      现在有一个想法就是：我不想阻塞主线程，实现一个WaitAll的操作
+     *          t1执行完成之后，执行t2,这就是延续的概念
+     *          延续 = 它的基础就是Wait...
+     *      - WhenAll
+     *      - WhenAny
+     *  TaskFactory
+     *      Task工厂中的一些延续操作
+     *      - ContinueWhenAll
+     *      - ContinueWhenAny
+     *  介绍Task的7种阻塞方式 +  延续 
+     *  学打组合拳，Task异步任务会写的非常漂亮
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Thread的阻塞 【Join】
+            // Demo_1();
+
+            // Task的阻塞—【WaitAll】
+            // Demo_2();
+
+            // Task的阻塞—【WaitAny】
+            // Demo_3();
+
+            // Task的阻塞与延续—【WhenAll，ContinueWith】
+            // Demo_4();
+
+            // Task的阻塞与延续—【WhenAny，ContinueWith】
+            // Demo_5();
+
+            // Task.Factory阻塞与延续—【ContinueWhenAll】
+            // Demo_6();
+
+            // Task.Factory阻塞与延续—【ContinueWhenAny】
+            // Demo_7();
+
+            Console.WriteLine("我是主线程");
+            Console.ReadKey();
+        }
+
+        public static void Demo_1()
+        {
+            Thread t1 = new Thread(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                Console.WriteLine("我是工作线程-1");
+            });
+
+            Thread t2 = new Thread(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                Console.WriteLine("我是工作线程-2");
+            });
+
+            t1.Start();
+            t2.Start();
+
+            // t1 && t2: WaitAll操作【相当于】
+            // t1 || t2: WaitAny操作【相当于】
+            t1.Join();
+            t2.Join();
+        }
+
+
+        public static void Demo_2()
+        {
+
+            Task task1 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                Console.WriteLine($"我是工作线程-1, {DateTime.Now}");
+            });
+
+            Task task2 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                Console.WriteLine($"我是工作线程-2, {DateTime.Now}");
+            });
+
+            task1.Start();
+            task2.Start();
+
+            Task.WaitAll(new[] { task1, task2 });
+        }
+
+        public static void Demo_3()
+        {
+
+            Task task1 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine($"我是工作线程-1, {DateTime.Now}");
+            });
+
+            Task task2 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(0.2));
+                Console.WriteLine($"我是工作线程-2, {DateTime.Now}");
+            });
+
+            task1.Start();
+            task2.Start();
+
+            Task.WaitAny(new[] { task1, task2 });
+        }
+
+        public static void Demo_4()
+        {
+
+            Task task1 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine($"我是工作线程-1, {DateTime.Now}");
+            });
+
+            Task task2 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine($"我是工作线程-2, {DateTime.Now}");
+            });
+
+            task1.Start();
+            task2.Start();
+
+            Task.WhenAll(task1, task2).ContinueWith(t =>
+            {
+                // 执行“工作线程-3”的内容
+                Console.WriteLine($"我是工作线程-3, {DateTime.Now}");
+            });
+        }
+
+        public static void Demo_5()
+        {
+
+            Task task1 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine($"我是工作线程-1, {DateTime.Now}");
+            });
+
+            Task task2 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine($"我是工作线程-2, {DateTime.Now}");
+            });
+
+            task1.Start();
+            task2.Start();
+
+            Task.WhenAny(task1, task2).ContinueWith(t =>
+            {
+                // 执行“工作线程-3”的内容
+                Console.WriteLine($"我是工作线程-3, {DateTime.Now}");
+            });
+        }
+
+        public static void Demo_6()
+        {
+
+            Task task1 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine($"我是工作线程-1, {DateTime.Now}");
+            });
+
+            Task task2 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine($"我是工作线程-2, {DateTime.Now}");
+            });
+
+
+            task1.Start();
+            task2.Start();
+
+            Task.Factory.ContinueWhenAll(new Task[] { task1, task2 }, (task) =>
+             {
+                 // 执行“工作线程-3”的内容
+                 Console.WriteLine($"我是工作线程-3, {DateTime.Now}");
+             });
+        }
+
+        public static void Demo_7()
+        {
+
+            Task task1 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine($"我是工作线程-1, {DateTime.Now}");
+            });
+
+            Task task2 = new Task(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine($"我是工作线程-2, {DateTime.Now}");
+            });
+
+
+            task1.Start();
+            task2.Start();
+
+            Task.Factory.ContinueWhenAny(new Task[] { task1, task2 }, (task) =>
+            {
+                // 执行“工作线程-3”的内容
+                Console.WriteLine($"我是工作线程-3, {DateTime.Now}");
+            });
+        }
+    }
+}
