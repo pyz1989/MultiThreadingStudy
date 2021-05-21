b0b2cdf453d64c86c6e78dfb0eda1dd487b73d64
pyz1989 <pengyuanzhi@gmail.com>
2021-05-21 13:14:54 +0800

    update Thread_线程开销

--- MultiThreading/01_Thread_线程开销/Program.cs
+++ MultiThreading/01_Thread_线程开销/Program.cs
@@ -7,25 +7,11 @@ namespace _01_Thread_线程开销
     {
         static void Main(string[] args)
         {
-            //Thread thread = new Thread(() =>
-            //{
+            Thread thread = new Thread(() =>
+            {
 
-            //});
-            //thread.Start();
-            //Console.ReadKey();
-            int temp = 1;
-            int mum = temp + 9;
-            Console.WriteLine(mum);
-
-            int a1 = 1;
-            a1 = a1 + mum;
-            Console.WriteLine(a1);
-            int s1 = 1;
-            s1 += mum;
-            Console.WriteLine(s1);
-
-           s1 = a1 + mum;
-            Console.WriteLine(s1);
+            });
+            thread.Start();
             Console.ReadKey();
         }
     }
